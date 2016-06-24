3c58bcc1a6bbdf47c346ea8e001dacbad1fea2f9
Msilini <msilini.naceur@gmail.com>
2016-06-24 12:38:24 +0000

    première commit sur gerrit

--- src/BackendBundle/Entity/Theme.php
+++ src/BackendBundle/Entity/Theme.php
@@ -5,9 +5,10 @@ namespace BackendBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="BackendBundle\Repository\ThemeRepository")
  * @ORM\Table(name="themes_enquette")
  * 
  */
@@ -28,12 +29,34 @@ class Theme
     private $name;
     
     
+    /**
+     * @ORM\OneToMany(targetEntity="Enquette", mappedBy="theme")
+     */
+    private $enquettes;
+
+    public function __construct()
+    {
+        $this->enquettes = new ArrayCollection();
+    }
+    
+    public function getId(){
+        return $this->id;
+    }
+    
+    public function setId($id){
+        $this->id = $id;
+    }
     
     public function setName($newName){
         $this->name = $newName;
     }
     
     public function getName(){
+        return $this->name;
+    }
+    
+    
+    public function __toString(){
         return $this->name;
     }
 }
