dae31deeb5fab6df978e55901b61142556640af1
Msilini <msilini.naceur@gmail.com>
2016-06-24 12:38:24 +0000

    première commit sur gerrit
    Change-Id: I745a57c640b0fbdde883a4fe82c550a6dcf55d5c

--- src/FrontBundle/Controller/FrontController.php
+++ src/FrontBundle/Controller/FrontController.php
@@ -3,11 +3,14 @@
 namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BackendBundle\Entity\ThemeRepository;
 
 class FrontController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontBundle:Front:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $themes = $em->getRepository('BackendBundle:Theme')->getAllOrderByName();
+        return $this->render('FrontBundle:Front:index.html.twig', array('themes'=>$themes));
     }
 }
